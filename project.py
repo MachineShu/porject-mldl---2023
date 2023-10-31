f2b9e5b7260a6bfb80a2eed4bc160e5abed250e3
MachineShu <452990436@qq.com>
2023-10-31 21:19:34 +0800

    这是Add_nums4

--- project.py
+++ project.py
@@ -15,6 +15,10 @@ def Add_nums2(a,b):
 def Add_nums3(a,b):
     return a/b+2
 
+def Add_nums4(a,b,c):
+    return a/b+2+c
+
 print(Add_nums(1,8))
 print(Add_nums2(12,8))
 print(Add_nums3(5,7))
+print(Add_nums4(5,6,8))
